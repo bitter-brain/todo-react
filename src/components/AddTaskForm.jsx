3a1530b2ddca338e1c70d0e665c99dba73426920
Boris Alekseev <borisalekseev95@gmail.com>
2025-12-10 13:16:02 +0200

    Initial commit

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,26 @@
+import Field from "./Field.jsx";
+import Button from "./Button.jsx";
+
+const AddTaskForm = (props) => {
+  const {
+    addTask,
+  } = props
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    addTask()
+  }
+
+  return (
+    <form className="todo__form" onSubmit={onSubmit}>
+      <Field
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+      />
+      <Button type="submit">Add</Button>
+    </form>
+  )
+}
+
+export default AddTaskForm
---
+++ src/components/Todo.jsx
@@ -0,0 +1,52 @@
+import AddTaskForm from "./AddTaskForm.jsx";
+import SearchTaskForm from "./SearchTaskForm.jsx";
+import TodoInfo from "./TodoInfo.jsx";
+import TodoList from "./TodoList.jsx";
+
+const Todo = () => {
+
+  const tasks = [
+    { id: 'task-1', title: 'Купить молоко', isDone: false },
+    { id: 'task-2', title: 'Погладить кота', isDone: true },
+  ]
+
+  const deleteAllTasks = () => {
+    console.log('elf')
+  }
+
+  const deleteTask = (taskId) => {
+    console.log(`Удаляем задачу с id: ${taskId}`)
+  }
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    console.log(`Задача ${taskId} ${isDone ? 'выполнена' : 'не выполнена'}`)
+  }
+
+  const filterTasks = (query) => {
+    console.log(`Поиск ${query}`)
+  }
+
+  const addTask = () => {
+    console.log('Задача добавлена')
+  }
+
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm addTask ={addTask}/>
+      <SearchTaskForm onSearchInput={filterTasks}/>
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone}) => isDone).length}
+        onDeleteAllButtonClick={deleteAllTasks}
+      />
+      <TodoList
+        tasks = {tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete}
+      />
+    </div>
+  )
+}
+
+export default Todo
