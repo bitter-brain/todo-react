ac3eff94d851ab25372b1a7eed2ea8a37680506a
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-11 19:34:47 +0200

    configure base path and routing for Vercel deployment

--- src/shared/api/tasks/index.js
+++ src/shared/api/tasks/index.js
@@ -1,8 +1,7 @@
 import localAPI from './local'
 import serverAPI from './server'
 
-const isLocal = import.meta.env.VITE_STATIC_BACKEND === 'true'
-
+const isLocal = import.meta.env.DEV
 const tasksAPI = isLocal ? localAPI : serverAPI
 
 export default tasksAPI
