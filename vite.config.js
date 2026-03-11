46c48192690745e56dd01489dbecec30d03fe40d
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-11 19:55:50 +0200

    switch tasks storage from json-server to localStorage

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
 
   return {
-    base: '/',
+    base: isProd ? '/todo-react/' : '/',
     plugins: [react()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
   }
 })
